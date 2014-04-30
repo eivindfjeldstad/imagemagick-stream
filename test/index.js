b0236b5f580145167878a4a81f24fc0b3eba6f1f
Eivind Fjeldstad <eivind.fjeldstad@gmail.com>
2014-04-30 20:15:54 +0200

    add some tests

---
+++ test/index.js
@@ -0,0 +1,136 @@
+var assert = require('assert');
+var im = require('../');
+
+describe('im()', function () {
+  it('should have an .args property', function () {
+    var img = im();
+    assert(Array.isArray(img.args));
+    assert(img.args.length == 0);
+  });
+  
+  it('should have an .input property', function () {
+    assert(im().input == '-');
+  });
+  
+  it('should have an .output property', function () {
+    assert(im().output == '-');
+  });
+  
+  describe('.spawn()', function () {
+    it('should call .spawn() with setImmediate', function (done) {
+      im().on('spawn', function (proc) {
+        assert(proc.stdin);
+        assert(proc.stdout);
+        done();
+      });
+    });
+  
+    it('should add input and output format to .args', function (done) {
+      var img = im();
+      img.on('spawn', function () {
+        assert(img.input == img.args[0]);
+        assert(img.output == img.args[img.args.length - 1]);
+        done();
+      });
+    });
+  });
+  
+  describe('.quality()', function () {
+    it('should set the quality option', function () {
+      var img = im().quality(90);
+      assert(img.args.length == 2);
+      assert(img.args[0] == '-quality');
+      assert(img.args[1] == '90');
+    });
+  });
+  
+  describe('.resize()', function () {
+    it('should set the resize option', function () {
+      var img = im().resize('200x200');
+      assert(img.args.length == 2);
+      assert(img.args[0] == '-resize');
+      assert(img.args[1] == '200x200');
+    });
+  });
+  
+  describe('.scale()', function () {
+    it('should set the scale option', function () {
+      var img = im().scale('200x200');
+      assert(img.args.length == 2);
+      assert(img.args[0] == '-scale');
+      assert(img.args[1] == '200x200');
+    });
+  });
+  
+  describe('.crop()', function () {
+    it('should set the crop option', function () {
+      var img = im().crop('200x200');
+      assert(img.args.length == 2);
+      assert(img.args[0] == '-crop');
+      assert(img.args[1] == '200x200');
+    });
+  });
+  
+  describe('.gravity()', function () {
+    it('should set the gravity option', function () {
+      var img = im().gravity('North');
+      assert(img.args.length == 2);
+      assert(img.args[0] == '-gravity');
+      assert(img.args[1] == 'North');
+    });
+  });
+  
+  describe('.thumbnail()', function () {
+    it('should set the thumbnail option', function () {
+      var img = im().thumbnail('200x200');
+      assert(img.args.length == 2);
+      assert(img.args[0] == '-thumbnail');
+      assert(img.args[1] == '200x200');
+    });
+  });
+  
+  describe('.autoOrient()', function () {
+    it('should set the auto-orient option', function () {
+      var img = im().autoOrient();
+      assert(img.args.length == 1);
+      assert(img.args[0] == '-auto-orient');
+    });
+  });
+  
+  describe('.type()', function () {
+    it('should set the type option', function () {
+      var img = im().type('jpg');
+      assert(img.args.length == 2);
+      assert(img.args[0] == '-type');
+      assert(img.args[1] == 'jpg');
+    });
+  });
+  
+  describe('.inputFormat()', function () {
+    it('should set the input format', function () {
+      var img = im().inputFormat('test');
+      assert(img.input == 'test:-');
+    });
+  });
+  
+  describe('.outputFormat()', function () {
+    it('should set the output format', function () {
+      var img = im().outputFormat('test');
+      assert(img.output == 'test:-');
+    });
+  });
+  
+  describe('.options()', function () {
+    it('should allow freehand arguments', function () {
+      var img = im().options({
+        'gaussian-blur': 0.05,
+        'interlace': 'Plane'
+      });
+      
+      assert(img.args[0] == '-gaussian-blur');
+      assert(img.args[1] == '0.05');
+      assert(img.args[2] == '-interlace');
+      assert(img.args[3] == 'Plane');
+    });
+  });
+});
