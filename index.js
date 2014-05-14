e431e9fee9c58ec85110135367c96c1c0e25a48a
Eivind Fjeldstad <eivind.fjeldstad@gmail.com>
2014-05-14 21:02:19 +0200

    refactor

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var PassThrough = require('stream').PassThrough;
-var spawn = require('child_process').spawn
-var isError = require('util').isError;
+var spawn = require('child_process').spawn;
 var Duplex = require('reduplexer');
+var util = require('util');
+var isError = util.isError;
+var inherit = util.inherits;
 var fs = require('fs');
 
 /**
@@ -35,211 +37,213 @@ function ImageMagick (src) {
   if (src) this.from(src);
   setImmediate(this.spawn);
 }
-  
-ImageMagick.prototype = {
-  __proto__: Duplex.prototype,
-  
-  /**
-   * Sets the input file format
-   *
-   * @param {String} args
-   * @api public
-   */
-  
-  inputFormat: function (args) {
-    this.input = args + ':-';
-    return this;
-  },
-  
-  /**
-   * Sets the output file format
-   *
-   * @param {String} args
-   * @api public
-   */
-  
-  outputFormat: function (args) {
-    this.output = args + ':-';
-    return this;
-  },
-  
-  /**
-   * Sets the `quality` option
-   *
-   * @param {String|Number} args
-   * @api public
-   */
-  
-  quality: function (args) {
-    this.args.push('-quality', args);
-    return this;
-  },
-  
-  /**
-   * Sets the `resize` option
-   *
-   * @param {String} args
-   * @api public
-   */
-   
-  resize: function (args) {
-    this.args.push('-resize', args);
-    return this;
-  },
-  
-  /**
-   * Sets the `scale` option
-   *
-   * @param {String} args
-   * @api public
-   */
-   
-  scale: function (args) {
-    this.args.push('-scale', args);
-    return this;
-  },
-  
-  /**
-   * Sets the `crop` option
-   *
-   * @param {String} args
-   * @api public
-   */
-  
-  crop: function (args) {    
-    this.args.push('-crop', args);
-    return this;
-  },
-  
-  /**
-   * Sets the `gravity` option
-   *
-   * @param {String} args
-   * @api public
-   */
-  
-  gravity: function (args) {
-    this.args.push('-gravity', args);
-    return this;
-  },
-  
-  /**
-   * Sets the `thumbnail` option
-   *
-   * @param {String} args
-   * @api public
-   */
-  
-  thumbnail: function (args) {
-    this.args.push('-thumbnail', args);
-    return this;
-  },
-  
-  /**
-   * Sets the `auto-orient` option
-   *
-   * @api public
-   */
-  
-  autoOrient: function () {
-    this.args.push('-auto-orient');
-    return this;
-  },
-  
-  /**
-   * Sets the `type` option
-   *
-   * @param {String} args
-   * @api public
-   */
-  
-  type: function (args) {
-    this.args.push('-type', args);
-    return this;
-  },
 
-  /**
-   * Passes additional arguments
-   *
-   * @param {Object} options
-   * @api public
-   */
+/**
+ * Inherit from `Duplex`
+ */
   
-  options: function (options) {    
-    Object.keys(options).forEach(function (key) {
-      var val = options[key];
-      this.args.push('-' + key);
-      if (val != null) this.args.push(val);
-    }, this);
-    
-    return this;
-  },
+inherit(ImageMagick, Duplex);
 
-  /**
-   * Read image data from path
-   *
-   * @param {String} path
-   * @api public
-   */
-   
-  from: function (path) {
-    var read = fs.createReadStream(path);
-    read.on('error', this.onerror);
-    read.pipe(this);
-    return this;
-  },
+/**
+ * Sets the input file format
+ *
+ * @param {String} args
+ * @api public
+ */
+
+ImageMagick.prototype.inputFormat = function (args) {
+  this.input = args + ':-';
+  return this;
+},
+
+/**
+ * Sets the output file format
+ *
+ * @param {String} args
+ * @api public
+ */
+
+ImageMagick.prototype.outputFormat = function (args) {
+  this.output = args + ':-';
+  return this;
+};
+
+/**
+ * Sets the `quality` option
+ *
+ * @param {String|Number} args
+ * @api public
+ */
+
+ImageMagick.prototype.quality = function (args) {
+  this.args.push('-quality', args);
+  return this;
+};
+
+/**
+ * Sets the `resize` option
+ *
+ * @param {String} args
+ * @api public
+ */
+ 
+ImageMagick.prototype.resize = function (args) {
+  this.args.push('-resize', args);
+  return this;
+};
+
+/**
+ * Sets the `scale` option
+ *
+ * @param {String} args
+ * @api public
+ */
+ 
+ImageMagick.prototype.scale = function (args) {
+  this.args.push('-scale', args);
+  return this;
+};
+
+/**
+ * Sets the `crop` option
+ *
+ * @param {String} args
+ * @api public
+ */
+
+ImageMagick.prototype.crop = function (args) {    
+  this.args.push('-crop', args);
+  return this;
+};
+
+/**
+ * Sets the `gravity` option
+ *
+ * @param {String} args
+ * @api public
+ */
+
+ImageMagick.prototype.gravity = function (args) {
+  this.args.push('-gravity', args);
+  return this;
+};
+
+/**
+ * Sets the `thumbnail` option
+ *
+ * @param {String} args
+ * @api public
+ */
+
+ImageMagick.prototype.thumbnail = function (args) {
+  this.args.push('-thumbnail', args);
+  return this;
+};
+
+/**
+ * Sets the `auto-orient` option
+ *
+ * @api public
+ */
+
+ImageMagick.prototype.autoOrient = function () {
+  this.args.push('-auto-orient');
+  return this;
+};
+
+/**
+ * Sets the `type` option
+ *
+ * @param {String} args
+ * @api public
+ */
+
+ImageMagick.prototype.type = function (args) {
+  this.args.push('-type', args);
+  return this;
+};
+
+/**
+ * Passes additional arguments
+ *
+ * @param {Object} options
+ * @api public
+ */
+
+ImageMagick.prototype.options = function (options) {    
+  Object.keys(options).forEach(function (key) {
+    var val = options[key];
+    this.args.push('-' + key);
+    if (val != null) this.args.push(val);
+  }, this);
   
-  /**
-   * Write image data to path
-   *
-   * @param {String} path
-   * @api public
-   */
-   
-  to: function (path) {
-    var write = fs.createWriteStream(path);
-    write.on('error', this.onerror);
-    this.pipe(write);
-    return this;
-  },
+  return this;
+};
+
+/**
+ * Read image data from path
+ *
+ * @param {String} path
+ * @api public
+ */
+ 
+ImageMagick.prototype.from = function (path) {
+  var read = fs.createReadStream(path);
+  read.on('error', this.onerror);
+  read.pipe(this);
+  return this;
+};
+
+/**
+ * Write image data to path
+ *
+ * @param {String} path
+ * @api public
+ */
+ 
+ImageMagick.prototype.to = function (path) {
+  var write = fs.createWriteStream(path);
+  write.on('error', this.onerror);
+  this.pipe(write);
+  return this;
+};
+
+/**
+ * Spawn `convert`
+ *
+ * @api private
+ */
+
+ImageMagick.prototype.spawn = function () {
+  this.args.push(this.output);
+  this.args.unshift(this.input);
   
-  /**
-   * Spawn `convert`
-   *
-   * @api private
-   */
+  var proc = spawn('convert', this.args);
   
-  spawn: function () {
-    this.args.push(this.output);
-    this.args.unshift(this.input);
-    
-    var proc = spawn('convert', this.args);
-    
-    var stdin = proc.stdin;  
-    stdin.on('error', this.onerror);
-    this.in.pipe(stdin);
-    
-    var stdout = proc.stdout;
-    stdout.on('error', this.onerror);
-    stdout.pipe(this.out);
-    
-    var stderr = proc.stderr;
-    stderr.on('data', this.onerror);
-    stderr.on('error', this.onerror);
+  var stdin = proc.stdin;  
+  stdin.on('error', this.onerror);
+  this.in.pipe(stdin);
   
-    this.emit('spawn', proc);
-  },
+  var stdout = proc.stdout;
+  stdout.on('error', this.onerror);
+  stdout.pipe(this.out);
   
-  /**
-   * Re-emit errors
-   *
-   * @param {Error|Buffer} err
-   * @api private
-   */
-  
-  onerror: function (err) {
-    if (!isError(err)) err = new Error(err);
-    if (!this.listeners('error')) throw err;
-    this.emit('error', err);
-  }
+  var stderr = proc.stderr;
+  stderr.on('data', this.onerror);
+  stderr.on('error', this.onerror);
+
+  this.emit('spawn', proc);
+};
+
+/**
+ * Re-emit errors
+ *
+ * @param {Error|Buffer} err
+ * @api private
+ */
+
+ImageMagick.prototype.onerror = function (err) {
+  if (!isError(err)) err = new Error(err);
+  if (!this.listeners('error')) throw err;
+  this.emit('error', err);
 };
